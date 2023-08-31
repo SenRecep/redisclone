eb4da6330e0b376a451104c00f24051127a3695d
recepsen <recepsen@taptoweb.com>
2023-08-31 15:57:40 +0300

    refactor: kvstorage set and update methods

--- src/internal/storage/memory/kvstorage/set.go
+++ src/internal/storage/memory/kvstorage/set.go
@@ -1,6 +1,18 @@
 package kvstorage
 
-func (ms *memoryStorage) Set(key string, value any) any {
+import (
+	"fmt"
+	"github.com/SenRecep/redisclone/src/internal/kverror"
+)
+
+func (ms *memoryStorage) Set(key string, value any) (any, error) {
+	if _, err := ms.Get(key); err == nil { // can not set! key already exist
+		return nil, fmt.Errorf("%w", kverror.ErrKeyExists.AddData("'"+key+"' exist"))
+	}
+	return ms.set(key, value), nil
+}
+
+func (ms *memoryStorage) set(key string, value any) any {
 	ms.mu.Lock()
 	defer ms.mu.Unlock()
 	ms.db[key] = value
